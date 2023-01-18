ebf7719f992c4b186e8ed025517a4c58c75fad4e
Vishnu Ganesan <vishnukg@gmail.com>
2023-01-18 21:16:11 +1100

    Added unit test for deposit

--- src/account/operations/deposit/deposit.test.js
+++ src/account/operations/deposit/deposit.test.js
@@ -17,3 +17,18 @@ tap.test('Dont allow negative deposits', (t) => {
     );
     t.end();
 });
+
+tap.test('Deposit money into account should increase the balance', (t) => {
+    const account = {
+        accountId: '123',
+        customer: {
+            id: '4563',
+            name: 'john',
+            email: 'john@email.com'
+        },
+        balance: 100
+    };
+    const result = deposit(account, 100);
+    t.equal(result.balance, 200);
+    t.end();
+});
