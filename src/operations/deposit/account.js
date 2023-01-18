648867e1ca1c67fb9432018b1b9a16b910876d61
Vishnu Ganesan <vishnukg@gmail.com>
2023-01-18 18:34:11 +1100

    Added deposit method

---
+++ src/operations/deposit/account.js
@@ -0,0 +1,10 @@
+export default (account, amount) => {
+    if (amount < 0) {
+        throw new Error('You cannot deposit negative amount');
+    }
+
+    return {
+        ...account,
+        balance: account.balance + amount
+    };
+};
