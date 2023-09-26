2713bd48f0a6c7dc38373ef35e6bef9debfe41a0
Vishnu Ganesan <vishnukg@gmail.com>
2023-09-26 22:14:32 +1000

    Added more tests

--- src/account/operations/deposit/deposit.test.mjs
+++ src/account/operations/deposit/deposit.test.mjs
@@ -1,33 +1,31 @@
-import { expect, test } from 'vitest';
-import deposit from './deposit.mjs';
+import { expect, test } from "vitest";
+import deposit from "./deposit.mjs";
 
-// tap.test('Dont allow negative deposits', (t) => {
-//     const account = {
-//         accountId: '123',
-//         customer: {
-//             id: '4563',
-//             name: 'john',
-//             email: 'john@email.com'
-//         },
-//         balance: 100
-//     };
-//     t.throws(
-//         () => deposit(account, -100),
-//         'You cannot deposit negative amount'
-//     );
-//     t.end();
-// });
-
-test('Deposit money into account should increase the balance', () => {
+test("Dont allow negative deposits", () => {
     const account = {
-        accountId: '123',
+        accountId: "123",
         customer: {
-            id: '4563',
-            name: 'john',
-            email: 'john@email.com'
+            id: "4563",
+            name: "john",
+            email: "john@email.com"
+        },
+        balance: 100
+    };
+    expect(() => {
+        deposit(account, -100);
+    }).toThrow("You cannot deposit negative amount");
+});
+
+test("Deposit money into account should increase the balance", () => {
+    const account = {
+        accountId: "123",
+        customer: {
+            id: "4563",
+            name: "john",
+            email: "john@email.com"
         },
         balance: 100
     };
     const result = deposit(account, 100);
-    expect(result.balance).toBe(200)
+    expect(result.balance).toBe(200);
 });
--- src/account/operations/withdraw/withdraw.test.mjs
+++ src/account/operations/withdraw/withdraw.test.mjs
@@ -1,54 +1,48 @@
-import {expect, test} from "vitest"
-import withdraw from './withdraw.mjs';
+import { expect, test } from "vitest";
+import withdraw from "./withdraw.mjs";
 
-test('Withdraw money from an account', () => {
+test("Withdraw money from an account", () => {
     const account = {
-        accountId: '123',
+        accountId: "123",
         customer: {
-            id: '4563',
-            name: 'John',
-            email: 'john@email.com'
+            id: "4563",
+            name: "John",
+            email: "john@email.com"
         },
         balance: 100
     };
 
     const result = withdraw(account, 100);
-    expect(result.balance).toBe(0)
+    expect(result.balance).toBe(0);
 });
 
-// tap.test(
-//     "Prevent customer from withdrawing more money that what's available",
-//     (t) => {
-//         const account = {
-//             accountId: '123',
-//             customer: {
-//                 id: '4563',
-//                 name: 'john',
-//                 email: 'john@email.com'
-//             },
-//             balance: 100
-//         };
-//         t.throws(
-//             () => withdraw(account, 140),
-//             'Not enough money to withdraw from the account'
-//         );
-//         t.end();
-//     }
-// );
-//
-// tap.test('Cannot withdraw negative money from account', (t) => {
-//     const account = {
-//         accountId: '123',
-//         customer: {
-//             id: '4563',
-//             name: 'john',
-//             email: 'john@email.com'
-//         },
-//         balance: 100
-//     };
-//     t.throws(
-//         () => withdraw(account, -150),
-//         'You cannot withdraw negative amount'
-//     );
-//     t.end();
-// });
+test("Prevent customer from withdrawing more money that what's available", () => {
+    const account = {
+        accountId: "123",
+        customer: {
+            id: "4563",
+            name: "john",
+            email: "john@email.com"
+        },
+        balance: 100
+    };
+    expect(() => {
+        withdraw(account, 140);
+    }).toThrow("Not enough money to withdraw from the account");
+});
+
+test("Cannot withdraw negative money from account", () => {
+    const account = {
+        accountId: "123",
+        customer: {
+            id: "4563",
+            name: "john",
+            email: "john@email.com"
+        },
+        balance: 100
+    };
+
+    expect(() => {
+        withdraw(account, -100);
+    }).toThrow("You cannot withdraw negative amount");
+});
